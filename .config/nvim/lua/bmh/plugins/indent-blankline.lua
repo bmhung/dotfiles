4987b5326ccc80fddad41d56bcd4704f9ca5eef5
bmhung <bui.m.hung@gmail.com>
2025-04-20 12:05:16 +0700

    Stop underline scope start

--- .config/nvim/lua/bmh/plugins/indent-blankline.lua
+++ .config/nvim/lua/bmh/plugins/indent-blankline.lua
@@ -1,8 +1,11 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  main = 'ibl',
-  opts = {
-    indent = { char = '▏' },
-  },
+	"lukas-reineke/indent-blankline.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	main = "ibl",
+	opts = {
+		indent = { char = "▏" },
+		scope = {
+			show_start = false,
+		},
+	},
 }
