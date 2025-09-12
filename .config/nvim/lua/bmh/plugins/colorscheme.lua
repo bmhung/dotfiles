1ed51b527524eefdb6108a2094d9d4d7548cd269
bmhung <bui.m.hung@gmail.com>
2025-09-12 14:14:31 +0700

    Update lsp-config

--- .config/nvim/lua/bmh/plugins/colorscheme.lua
+++ .config/nvim/lua/bmh/plugins/colorscheme.lua
@@ -1,10 +1,19 @@
 return {
   {
-    "ellisonleao/gruvbox.nvim",
+    'ellisonleao/gruvbox.nvim',
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- load the colorscheme here
       vim.cmd([[colorscheme gruvbox]])
     end,
   },
+  -- {
+  --   'darianmorat/gruvdark.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     vim.cmd.colorscheme('gruvdark')
+  --   end,
+  -- },
 }
