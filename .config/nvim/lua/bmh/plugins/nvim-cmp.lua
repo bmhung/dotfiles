f8705986f9db888facd4661ae048f1cd6bbb2018
bmhung <bui.m.hung@gmail.com>
2026-02-22 22:37:39 +0700

    Fix snippets

--- .config/nvim/lua/bmh/plugins/nvim-cmp.lua
+++ .config/nvim/lua/bmh/plugins/nvim-cmp.lua
@@ -23,6 +23,7 @@ return {
     local lspkind = require('lspkind')
 
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+    require('luasnip.loaders.from_vscode').lazy_load()
     require('luasnip.loaders.from_vscode').lazy_load({
       paths = { vim.fn.expand('~/.config/nvim/snippets/') },
     })
